c498958a2d2e56d94e70a4776cd6cff22b306bed
Dmlehan <dmlehanamanda@gmail.com>
2023-08-11 12:21:09 +0530

    Initial commit

---
+++ HibernateDay1/src/main/java/Lehan/org/util/FactoryConfiguration.java
@@ -0,0 +1,10 @@
+package Lehan.org.util;
+
+import org.hibernate.Session;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+
+    private Session session;
+
+}
